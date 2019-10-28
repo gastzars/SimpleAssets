f3dc8b0afe7d73a44347ae0efb093af8e7de963d
Tanapat Sainak <fallen_things@hotmail.com>
2019-10-28 15:34:31 +0700

    try remove code

---
+++ build/CMakeFiles/SimpleAssets_project.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleAssets_project"
+  "CMakeFiles/SimpleAssets_project-complete"
+  "SimpleAssets_project-prefix/src/SimpleAssets_project-stamp/SimpleAssets_project-build"
+  "SimpleAssets_project-prefix/src/SimpleAssets_project-stamp/SimpleAssets_project-configure"
+  "SimpleAssets_project-prefix/src/SimpleAssets_project-stamp/SimpleAssets_project-download"
+  "SimpleAssets_project-prefix/src/SimpleAssets_project-stamp/SimpleAssets_project-install"
+  "SimpleAssets_project-prefix/src/SimpleAssets_project-stamp/SimpleAssets_project-mkdir"
+  "SimpleAssets_project-prefix/src/SimpleAssets_project-stamp/SimpleAssets_project-patch"
+  "SimpleAssets_project-prefix/src/SimpleAssets_project-stamp/SimpleAssets_project-test"
+  "SimpleAssets_project-prefix/src/SimpleAssets_project-stamp/SimpleAssets_project-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/SimpleAssets_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/SimpleAssets/CMakeFiles/SimpleAssets.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleAssets.dir/SimpleAssets.obj"
+  "SimpleAssets.pdb"
+  "SimpleAssets.wasm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleAssets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
